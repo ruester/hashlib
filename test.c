8051cc3dbb4d82b1d721a74db56dc82a1b86ad01
Matthias Ruester <ruester@molgen.mpg.de>
2012-09-25 21:18:04 +0200

    tests: update test program

--- test.c
+++ test.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
 
 #include "hashlib.h"
 
 #define put(str) fputs((str), stdout)
+
+#define TEST(str) put("Testing " str ": ")
 
 struct translation {
     char *english;
@@ -11,10 +15,42 @@ struct translation {
     char *latin;
 };
 
+struct translation *translation_new(void)
+{
+    struct translation *t;
+
+    t = calloc(1, sizeof(*t));
+
+    if (!t)
+        err(EXIT_FAILURE, "calloc");
+
+    return t;
+}
+
+void translation_delete(void *a)
+{
+    struct translation *t;
+
+    t = (struct translation *) a;
+
+    free(t);
+}
+
+void failed(void)
+{
+    puts("failed");
+}
+
+void success(void)
+{
+    puts("success");
+}
+
 int main(void)
 {
     struct translation example;
     struct translation *p;
+    struct translation *r;
     struct hashlib_hash *hash;
 
     example.english = "horse";
@@ -31,21 +67,51 @@ int main(void)
 
     p = hashlib_get(hash, "horse");
 
-    put("Testing hashlib_get: ");
+    TEST("hashlib_get");
 
     if (p != &example)
         puts("failed");
     else
         puts("success");
 
-    put("Testing hashlib_count: ");
+    TEST("hashlib_count");
 
     if (hashlib_count(hash) != 4)
-        puts("failed");
+        failed();
     else
-        puts("success");
+        success();
+
+    hashlib_remove(hash, "horse");
+    hashlib_remove(hash, "Pferd");
+    hashlib_remove(hash, "equus");
+    hashlib_remove(hash, "cheval");
+
+    TEST("hashlib_remove");
+
+    p = translation_new();
+
+    hashlib_put(hash, "test", p);
+
+    r = hashlib_remove(hash, "test");
+
+    if (r != p) {
+        failed();
+    } else {
+        if (hashlib_get(hash, "test"))
+            failed();
+        else
+            success();
+    }
+
+    TEST("hashlib_hash_delete");
+
+    hashlib_set_free_function(hash, translation_delete);
+
+    hashlib_put(hash, "test", p);
 
     hashlib_hash_delete(hash);
+
+    success();
 
     return 0;
 }
