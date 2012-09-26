d8b9be874e536984a03b2286cbd34fb671a94bbd
Matthias Ruester <ruester@molgen.mpg.de>
2012-09-26 11:45:38 +0200

    tests: implement each test in a separate function

--- test.c
+++ test.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <err.h>
+#include <ctype.h>
+#include <string.h>
 
 #include "hashlib.h"
 
@@ -14,6 +16,61 @@ struct translation {
     char *french;
     char *latin;
 };
+
+
+int read_value(char *line)
+{
+    size_t len;
+
+    while (!isdigit(*line))
+        line++;
+
+    len = strlen(line);
+
+    /* cut off ' kb' */
+    line[len - 3] = '\0';
+
+    return atoi(line);
+}
+
+unsigned long read_VmRSS(void)
+{
+    FILE* file;
+    unsigned long result;
+    char line[BUFSIZ];
+
+    result = 0;
+    file   = fopen("/proc/self/status", "r");
+
+    if (!file)
+        err(EXIT_FAILURE, "fopen");
+
+    while (fgets(line, BUFSIZ, file)) {
+        if (!strncmp(line, "VmRSS:", 6)) {
+            result = read_value(line);
+            break;
+        }
+    }
+
+    if (fclose(file) == EOF)
+        err(EXIT_FAILURE, "fclose");
+
+    return result * 1024L;
+}
+
+unsigned long get_VmRSS(void)
+{
+    unsigned long old, value;
+
+    old = value = read_VmRSS();
+
+    do {
+        old   = value;
+        value = read_VmRSS();
+    } while (old != value);
+
+    return value;
+}
 
 struct translation *translation_new(void)
 {
@@ -46,11 +103,10 @@ void success(void)
     puts("success");
 }
 
-int main(void)
+void test1(void)
 {
     struct translation example;
-    struct translation *p;
-    struct translation *r;
+    struct translation *a, *b, *c, *d, *p;
     struct hashlib_hash *hash;
 
     example.english = "horse";
@@ -65,34 +121,56 @@ int main(void)
     hashlib_put(hash, "equus",  &example);
     hashlib_put(hash, "cheval", &example);
 
-    p = hashlib_get(hash, "horse");
+    a = hashlib_get(hash, "horse");
+    b = hashlib_get(hash, "Pferd");
+    c = hashlib_get(hash, "equus");
+    d = hashlib_get(hash, "cheval");
+
+    p = &example;
 
     TEST("hashlib_get");
 
-    if (p != &example)
+    if (a != p || b != p || c != p || d != p)
         puts("failed");
     else
         puts("success");
 
+    hashlib_hash_delete(hash);
+}
+
+void test2(void)
+{
+    struct translation a, b;
+    struct hashlib_hash *hash;
+
+    hash = hashlib_hash_new(1000);
+
+    hashlib_put(hash, "one", &a);
+    hashlib_put(hash, "two", &b);
+
     TEST("hashlib_count");
 
-    if (hashlib_count(hash) != 4)
+    if (hashlib_count(hash) != 2)
         failed();
     else
         success();
 
-    hashlib_remove(hash, "horse");
-    hashlib_remove(hash, "Pferd");
-    hashlib_remove(hash, "equus");
-    hashlib_remove(hash, "cheval");
+    hashlib_hash_delete(hash);
+}
 
-    TEST("hashlib_remove");
+void test3(void)
+{
+    struct hashlib_hash *hash;
+    struct translation *p, *r;
 
-    p = translation_new();
+    hash = hashlib_hash_new(1000);
+    p    = translation_new();
 
     hashlib_put(hash, "test", p);
 
     r = hashlib_remove(hash, "test");
+
+    TEST("hashlib_remove");
 
     if (r != p) {
         failed();
@@ -103,15 +181,79 @@ int main(void)
             success();
     }
 
-    TEST("hashlib_hash_delete");
+    translation_delete(p);
+    hashlib_hash_delete(hash);
+}
+
+void test4(void)
+{
+    struct hashlib_hash *hash;
+    unsigned long before, after;
+    struct translation *p;
+
+    hash = hashlib_hash_new(1000);
+    p    = translation_new();
 
     hashlib_set_free_function(hash, translation_delete);
 
     hashlib_put(hash, "test", p);
 
+    before = get_VmRSS();
+
+    hashlib_remove(hash, "test");
+
+    after = get_VmRSS();
+
+    TEST("hashlib_remove with free_function");
+
+    /* this does not work properly yet */
+    if (after > before || hashlib_get(hash, "test"))
+        failed();
+    else
+        success();
+
+    hashlib_hash_delete(hash);
+}
+
+void test5(void)
+{
+    unsigned long before, after;
+    struct hashlib_hash *hash;
+    struct translation *p, *q;
+
+    before = get_VmRSS();
+    hash   = hashlib_hash_new(1000);
+    p      = translation_new();
+    q      = translation_new();
+
+    hashlib_set_free_function(hash, translation_delete);
+
+    hashlib_put(hash, "foo", p);
+    hashlib_put(hash, "bar", q);
+
     hashlib_hash_delete(hash);
 
-    success();
+    after = get_VmRSS();
+
+    TEST("hashlib_hash_delete");
+
+    if (after != before)
+        failed();
+    else
+        success();
+}
+
+#define TESTS 5
+
+int main(void)
+{
+    int i;
+    void (*arr[TESTS])(void) = {
+        test1, test2, test3, test4, test5
+    };
+
+    for (i = 0; i < TESTS; i++)
+        arr[i]();
 
     return 0;
 }
